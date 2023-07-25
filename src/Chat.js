26c208176643da3560d26709813b438a305a4af9
Rishabh-code-do <rishabhjais01234@gmail.com>
2023-07-25 22:51:12 +0530

    initial3

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
@@ -10,8 +10,7 @@ import ChatInput from './ChatInput';
 function Chat() {
   const {roomId} = useParams();
   const [roomDetails,setRoomDetails] = useState(null);
-  const [roomMessages,setRoomMessages] = useState([])
-//   const divUnderMessages = useRef();
+  const [roomMessages,setRoomMessages] = useState([]);
 
   useEffect(()=>{
     if(roomId)
@@ -62,7 +61,6 @@ function Chat() {
             userImage={userImage}/>
         ))}
       </div>
-      {/* <div ref={divUnderMessages}></div> */}
       <ChatInput channelName={roomDetails?.name} channelId={roomId}/>
     </div>
   )
